38f00147a2ef31f4fa642bb1b6cdff687a8f4217
Def Lack <azargasanov13@yandex.ru>
2019-11-01 15:36:13 +0300

    Realization of program

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class Program
+    abstract class Figure
     {
-        static void Main(string[] args)
-        {
+        public virtual double GetArea() 
+        { 
+            return 0;
         }
+    }
+    class Rectangle : Figure, IPrint
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+        public override string ToString()
+        {
+            return "Width = " + Width + ", height = " + Height + ", area = " + GetArea();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+    class Square : Rectangle, IPrint
+    {
+        public Square(double length) : base(length, length) {}
+        public override string ToString()
+        {
+            return "Length = " + Width + ", area = " + GetArea();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+    class Circle : Figure, IPrint
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public override string ToString()
+        {
+            return "Radius = " + Radius + ", area = " + GetArea();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+    public static class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+    interface IPrint
+    {
+        void Print();
     }
 }
