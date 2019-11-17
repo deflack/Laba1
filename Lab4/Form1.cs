82c47d7d4ccb8d1360d5062ac7eaa7c2996faab0
Def Lack <azargasanov13@yandex.ru>
2019-11-17 21:32:48 +0300

    Adding LR4

---
+++ Lab4/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        List<string> list = new List<string>();
+        Stopwatch sw = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files|*.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                list.Clear();
+                string[] words = File.ReadAllText(ofd.FileName).Split('\n', ' ', ',', '.', '?', ';', ':', '(', ')', '"', '!');
+                string text = "";
+                sw.Reset();
+                sw.Start();
+                foreach(string word in words)
+                {
+                    if (!text.Contains(word))
+                        list.Add(word);
+                    text += word;
+                }
+                sw.Stop();
+                text = "";
+                foreach (string item in list)
+                    text += " " + item;
+                label1.Text = "Время сохранения слов в список, мс: " + sw.ElapsedMilliseconds.ToString();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //List<string> searchedWords = new List<string>();
+            if (list.Count == 0)
+                MessageBox.Show("Файл не прочитан либо не содержит текст.");
+            else if (textBox1.Text.Length == 0)
+                MessageBox.Show("Введите слово, которое хотите найти.");
+            else
+            {
+                listBox1.BeginUpdate();
+                listBox1.Items.Clear();
+                sw.Reset();
+                sw.Start();
+                foreach(string item in list)
+                {
+                    if (item.Contains(textBox1.Text)) listBox1.Items.Add(item);
+                }
+                sw.Stop();
+                listBox1.EndUpdate();
+                label2.Text = "Время поиска слов, мс: " + sw.ElapsedMilliseconds.ToString();
+            }
+        }
+    }
+}
